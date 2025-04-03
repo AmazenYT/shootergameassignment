5b1f88b9f244e9079de265eb6f8fd30d4057b68a
AmazenYT <frankiecahill90@gmail.com>
2025-04-03 15:33:11 +0100

    Enemy animations work

--- shooter-game/enemy.gd
+++ shooter-game/enemy.gd
@@ -10,28 +10,29 @@ var is_healed = false  # Tracks if the enemy has been "healed"
 @onready var anim = $AnimationPlayer  # Animation controller
 @onready var area = $Area3D  # Bullet detection area
 
-func _ready():
-    area.body_entered.connect(_on_bullet_hit)  # Connects bullet detection
-
 func _physics_process(delta):
-    if player:
-        nav_agent.target_position = player.global_transform.origin  # Set target to player
+	if player:
+		nav_agent.target_position = player.global_transform.origin  # Set target to player
 
-    if not is_healed:  # Enemy crawls normally
-        move_towards_target(delta, speed)
-    else:  # Moves faster when "healed"
-        move_towards_target(delta, healed_speed)
+	if not is_healed:  # Enemy crawls normally
+		move_towards_target(delta, speed)
+	else:  # Moves faster when "healed"
+		move_towards_target(delta, healed_speed)
 
 func move_towards_target(delta, move_speed):
-    var next_path_position = nav_agent.get_next_path_position()
-    var direction = (next_path_position - global_transform.origin).normalized()
-    
-    velocity = direction * move_speed
-    move_and_slide()
+	var next_path_position = nav_agent.target_position
+	var direction = (next_path_position - global_transform.origin).normalized()
+	
+	velocity = direction * move_speed
+	move_and_slide()
 
-func _on_bullet_hit(body):
-    if body.is_in_group("bullet") and not is_healed:
-        is_healed = true  # Mark enemy as healed
-        anim.play("stand_up")  # Play stand-up animation
-        await anim.animation_finished  # Wait for the animation to finish
-        queue_free()  # Remove enemy after standing up
+
+
+func _on_area_3d_area_entered(area: Area3D) -> void:
+	print(area)
+	if area.is_in_group("bullet") and not is_healed:
+		is_healed = true  # Mark enemy as healed
+		anim.play("StandingUp(2)0")  # Play stand-up animation
+		await anim.animation_finished  # Wait for the animation to finish
+		queue_free()  # Remove enemy after standing up
+	pass # Replace with function body.
